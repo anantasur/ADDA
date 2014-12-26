7906ef09bf61bdc36de493627ad2200aae5d448a
vikaspal <vikaspal@thoughtworks.com>
2014-12-26 17:33:04 +0530

    modified shell script for stop filling production db Added mocha in package.json

--- scripts/db_creator.sh
+++ scripts/db_creator.sh
@@ -3,8 +3,8 @@ rm -rf data/adda.db
 rm -rf tests/data/adda.db
 rm -rf tests/data/adda.db.backup
 echo "------------->>>initializing production db"
+mkdir data
 node scripts/initialize_db.js data/adda.db
-sqlite3 data/adda.db < scripts/fill_sample_data.sql
 echo "-------------->>>initializing test db"
 node tests/scripts/initialize_db.js tests/data/adda.db.backup
 sqlite3 tests/data/adda.db.backup < tests/scripts/fill_sample_data.sql
